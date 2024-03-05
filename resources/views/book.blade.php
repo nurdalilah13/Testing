1211ce43d6d41e84a45a8e06d307ff5b4cf958ac
dalilah <nurdalilahmohdzamri8@gmail.com>
2024-03-05 16:29:29 +0800

    first

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BOOK</title>
-</head>
-
-<body>
+@section('content')
     <h1>Hello</h1>
     <form method="POST" action="{{ route('books.store') }}">
         @csrf
@@ -56,6 +48,4 @@
         </tbody>
     </table>
     <a href="{{ route('home.laravel') }}">Welcome</a>
-</body>
-
-</html>
+@endsection
